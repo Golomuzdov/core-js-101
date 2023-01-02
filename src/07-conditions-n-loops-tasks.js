f4a2531e5498802dce49feffb4d2891ff5dfae2c
Golomuzdov <anton210027@gmail.com>
2023-01-03 00:51:10 +0300

    feat: add digital-root function

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -321,8 +321,11 @@ function isCreditCardNumber(ccn) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  if (num < 10) {
+    return num;
+  }
+  return getDigitalRoot(+String(num).split('').reduce((el, acc) => +acc + +el));
 }
 
 
