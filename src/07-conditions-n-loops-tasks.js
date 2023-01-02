667daa7734711f941c684a65d338258e92486c72
Golomuzdov <anton210027@gmail.com>
2023-01-03 00:46:10 +0300

    feat: add get-interval-string function

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -230,8 +230,10 @@ function findFirstSingleChar(str) {
  *   5, 3, true, true   => '[3, 5]'
  *
  */
-function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
-  throw new Error('Not implemented');
+function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
+  const one = isStartIncluded ? '[' : '(';
+  const two = isEndIncluded ? ']' : ')';
+  return `${one + Math.min(a, b)}, ${Math.max(a, b)}${two}`;
 }
 
 
